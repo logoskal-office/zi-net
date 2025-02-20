b86f3f6c35ba8107b8a15dbfeb207bf82f0a0a67
Logos <132839177+logoskal@users.noreply.github.com>
2025-02-20 22:46:19 +0300

    Home Page Change

--- zi_net/display/urls.py
+++ zi_net/display/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from display import views as display_views
 
 urlpatterns = [
-    path('', display_views.home, name='landing-page'),
+    path('', display_views.landing, name='landing-page'),
     path('home/', display_views.home, name='home-page'),
     path('test/', display_views.test, name='test-page'),
     path('contact-us/', display_views.contact_us, name='contact-us-page'),
     path('about-us/', display_views.about_us, name='about-us-page'),
+    path('landing/', display_views.landing, name='landing-page'),
 ]
